9a1edac8d683d3bce17223993fd2447017ea2c28
Joon <pss21242124@gmail.com>
2024-07-15 23:59:34 +0900

    24.07.15

---
+++ src/ch13/sec04/GenericExample.java
@@ -0,0 +1,9 @@
+package ch13.sec04;
+
+public class GenericExample {
+    //제한된 타입 파라미터를 갖는 제네릭 메소드
+    public static <T extends Number> boolean compare(T t1, T t2) {
+
+        return a.equals(b);
+    }
+}
